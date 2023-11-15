e94536a0a57e01a09f1992c4d83a103e69b8a55a
aminenejip <aminenejip@gmail.com>
2023-11-15 15:25:52 +0100

    first

---
+++ c/home(test)/switch.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    char day='7';
+    switch(day){
+case '1':
+    printf("qjkqgdj");
+    break;
+case '2':
+    printf("gzhdgsh");
+    break;
+default:
+    printf("7");
+    }
+    return 0;
+
+
+}
---
+++ c/test.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main() {
+    // Your C code here
+    return 0;
+}
